596f2e7dfdcccc4fde0667f9d72e149dc118f11a
AGibson17 <andrew.j.gibs@gmail.com>
2025-06-25 19:39:09 +0000

    starting over

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-red-600 text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-6xl font-bold">Tailwind Test</h1>
-    </div>
-  );
-}
